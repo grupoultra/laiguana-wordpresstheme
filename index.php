3faa4f11de1179936453e55993ced1562af09650
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2017-07-25 15:51:37 -0400

    primer acercamiento al index

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<p>Hello world</p>
+<?php get_header() ?>
 
-<?php get_header(); ?>
+<?php
+    if(is_category( )){
+        echo "Estas en una categoria";
+    } elseif(is_home()) {
+        // echo "Este es el cuerpo de la página";
+    } else {
+        echo "No se donde estás";
+    }
+?>
+
+<?php get_template_part( "destacados")?>
+
+
+<?php get_footer() ?>

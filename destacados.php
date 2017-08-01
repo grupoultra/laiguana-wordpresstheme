15611ff104be9f0b217ef8c88f78ddf1212e0647
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2017-08-01 18:15:03 -0400

    implementación del carrusel con bootstrap

--- destacados.php
+++ destacados.php
@@ -1,3 +1,28 @@
+<!-- <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="la.jpg" alt="Los Angeles">
+    </div>
+
+    <div class="item">
+      <img src="chicago.jpg" alt="Chicago">
+    </div>
+
+    <div class="item">
+      <img src="ny.jpg" alt="New York">
+    </div>
+  </div>
+
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div> -->
+
 <div class="row">
         <?php
             $aboutPosts = new WP_Query(array(
@@ -6,11 +31,14 @@
             ));
 
             if ($aboutPosts->have_posts()) : ?>
-                <div class="owl-carousel-destacados owl-carouse">
+                <div id="myCarousel" class="carousel slide owl-carousel-destacados owl-carouel"  data-ride="carousel">
+                <div class="carousel-inner">
+
                 <?php 
+                $destacado_index = 0;
                 while ($aboutPosts->have_posts()) : $aboutPosts->the_post();?>  
 
-                <div class="destacado-item">
+                <div class="destacado-item item <?php if ($destacado_index == 0):?> active <?php endif; ?>">
                     <a href="<?php the_permalink(); ?>">
                         <?php the_post_thumbnail('destacado'); ?>
                     </a>
@@ -23,7 +51,19 @@
                     </div>
                 </div>
 
-        <?php  endwhile; ?>
+        <?php
+            $destacado_index++;  
+            endwhile; ?>
+                </div>
+
+<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
                 </div>
         <?php
             endif;

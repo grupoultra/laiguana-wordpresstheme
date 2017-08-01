20f9370c87db567259f28186c6314cd64e37e54b
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2017-08-01 18:13:27 -0400

    backup de destacados con carrusel de owl

---
+++ destacados_bk.php
@@ -0,0 +1,33 @@
+<div class="row">
+        <?php
+            $aboutPosts = new WP_Query(array(
+                'category_name' => 'Destacados',
+                'posts_per_page' => 4,
+            ));
+
+            if ($aboutPosts->have_posts()) : ?>
+                <div class="owl-carousel-destacados owl-carouel">
+                <?php 
+                while ($aboutPosts->have_posts()) : $aboutPosts->the_post();?>  
+
+                <div class="destacado-item">
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_post_thumbnail('destacado'); ?>
+                    </a>
+                    <div class="destacado-caption">
+                        <a class="destacado-title-a " href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                            <?php the_title()?>
+                        </a>
+
+                        <p class="destacado-date">Publicada el <?php echo get_the_date()?></p>
+                    </div>
+                </div>
+
+        <?php  endwhile; ?>
+                </div>
+        <?php
+            endif;
+            wp_reset_query();
+        ?>
+</div>
+

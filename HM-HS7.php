5a895eed24827cccf5a71e69012d21874611f7a1
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2017-08-01 09:57:43 -0400

    Adicion de banners

---
+++ HM-HS7.php
@@ -0,0 +1,11 @@
+<div id="HM-HS7" class="banner margin-top-10 banner-c banner320x50 hidden-desktop">
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- 320x50 -->
+    <ins class="adsbygoogle"
+         style="display:inline-block;width:320px;height:50px"
+         data-ad-client="ca-pub-8216476634474264"
+         data-ad-slot="1646640634"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+</div>
---
+++ ND-C1.php
@@ -0,0 +1,13 @@
+<div id="ND-C1" class="margin-top-10 banner banner300x250 hidden-mobile">
+   <script type="text/javascript"><!--
+   google_ad_client = "ca-pub-4903453974745530";
+   /* laiguana.tv | web | 300x250 | */
+   google_ad_slot = "5677479066";
+   google_ad_width = 300;
+   google_ad_height = 250;
+   //-->
+   </script>
+   <script type="text/javascript"
+   src="//pagead2.googlesyndication.com/pagead/show_ads.js">
+   </script>
+</div>

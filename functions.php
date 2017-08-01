d1fd86069e77d2d156825c743e76c8e509d00f76
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2017-08-01 10:00:16 -0400

    anadiendo tamanos de thumbnails, funciones para contar visitas, obtener el video de un post

--- functions.php
+++ functions.php
@@ -27,11 +27,65 @@ function laiguana_setup() {
 	// Add featured image support
 	add_theme_support('post-thumbnails');
 	add_image_size('destacado', 1070, 441, true);
-	add_image_size('square-thumbnail', 80, 80, true);
-	add_image_size('banner-image', 920, 210, array('left', 'top'));
-	
+	add_image_size('home_thumbnail_medium', 325, 244, true);
+	add_image_size('home_thumbnail_small', 259, 106, true);
+	add_image_size('iguanazos', 728, 421, true);
+	add_image_size('iguanazos_anteriores', 203, 130, true);
+	add_image_size('lo_mas_jot', 278, 80, true);
+    
 	// Add post type support
 	add_theme_support('post-formats', array('aside', 'gallery', 'link'));
 }
 
 add_action('after_setup_theme', 'laiguana_setup');
+
+
+//  from http://www.wpbeginner.com/wp-tutorials/how-to-track-popular-posts-by-views-in-wordpress-without-a-plugin/
+function wpb_set_post_views($postID) {
+    $count_key = 'wpb_post_views_count';
+    $count = get_post_meta($postID, $count_key, true);
+    if($count==''){
+        $count = 0;
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
+    }else{
+        $count++;
+        update_post_meta($postID, $count_key, $count);
+    }
+}
+//To keep the count accurate, lets get rid of prefetching
+remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+
+function wpb_track_post_views ($post_id) {
+    if ( !is_single() ) return;
+    if ( empty ( $post_id) ) {
+        global $post;
+        $post_id = $post->ID;    
+    }
+    wpb_set_post_views($post_id);
+}
+add_action( 'wp_head', 'wpb_track_post_views');
+
+function wpb_get_post_views($postID){
+    $count_key = 'wpb_post_views_count';
+    $count = get_post_meta($postID, $count_key, true);
+    if($count==''){
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
+        return "0 Vistas";
+    }
+    return $count.' Vistas';
+}
+
+function the_post_video($post_id=NULL) {
+   global $post;
+   $target_post = $post;
+   if($post_id !== NULL)
+       $target_post = get_post($post_id);
+
+   $matches = null;
+   if(preg_match('/<iframe(.*?)\\/?>(<\\/iframe>)?/s', $post->content, $matches)) {
+      return $matches[0];
+   }
+   return ''; // return empty if no iframe found.
+}

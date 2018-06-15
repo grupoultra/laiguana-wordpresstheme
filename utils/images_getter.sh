29f49dd5106c21985d221a10a73940de0614ead1
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-06-15 15:10:02 -0400

    Mocker utilities

---
+++ utils/images_getter.sh
@@ -0,0 +1,10 @@
+mkdir ./wp-content/uploads/2017/08
+cd $_
+
+wget http://placehold.it/1070x441.png /
+cp 1070x441.png 1070x441-203x130.png
+cp 1070x441.png 1070x441-278x80.png
+cp 1070x441.png 1070x441-328x135.png
+cp 1070x441.png 1070x441-259x106.png
+cp 1070x441.png 1070x441-325x244.png
+cp 1070x441.png 1070x441-728x421.png

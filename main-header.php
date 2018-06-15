7f60e883e9c0ef849912e57bd8302446ee025c64
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-06-15 17:00:32 -0400

    nuevas imagenes

--- main-header.php
+++ main-header.php
@@ -1,3 +1,35 @@
+<header>
+    <div class="row mobile-header hidden-desktop">
+        <a href="/">
+            <img src="/activos/assets/img/li_mobile_head.jpg" alt="La Iguana">
+        </a>
+
+        <div class="row social-box center-block text-center">
+            <#include "social-nav.ftl">
+        </div>
+    </div>
+
+    <div class="row main-header hidden-mobile">
+        <a id="titlelink" href="/"></a>
+        <div class="text-right twitter-follow">
+
+            <a  title="Sigue a LaIguanaTV (@la_iguanatv) en Twitter"
+                href="https://twitter.com/intent/follow?&amp;ref_src=twsrc%5Etfw&amp;region=follow_link&amp;screen_name=la_iguanatv&amp;tw_p=followbutton">
+                <img src="/activos/assets/img/twitter-seguir.png" alt="La Iguana">
+            </a>
+        </div>
+
+        <#include "search-box.ftl">
+
+        <div class="social-box">
+            <#include "social-nav.ftl">
+        </div>
+    </div>
+</header>
+
+
+
+
 	<div class="row mobile-header">
 		<a href="<?php echo home_url(); ?>">
 			<img src="<?php echo get_bloginfo('template_url') ?>/assets/img/li_mobile_head.png" style="width: 50%; height: auto;" alt="La Iguana">

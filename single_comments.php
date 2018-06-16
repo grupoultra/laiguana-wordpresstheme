6b7c916a00bc5eedad9aa71e9021fb0d8cbbc9f2
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-06-16 11:01:47 -0400

    update comments layout

--- single_comments.php
+++ single_comments.php
@@ -1,11 +1,12 @@
+
 <div class="col-g articulo">
     <div class="row">
         <div class="col-xs-12 title-span  margin-top-10">
-            <div class="col-xs-2">
-                <img class="clean img-responsive" src="<?php echo get_bloginfo('template_url') ?>/assets/img/comentario.png" style="padding: 0; margin: 0;" alt="La Iguana comentarios">
+            <div class="banner-c banner728x90 hidden-mobile">
+                <img class="clean img-responsive" src="<?php echo get_bloginfo('template_url') ?>/assets/img/comentarios.jpg" style="padding: 0; margin: 0;" alt="La Iguana comentarios">
             </div>
-            <div class="col-xs-10">
-                <span class="clean">Comentarios</span>
+            <div class="margin-bottom-10 banner-c banner320x50 hidden-desktop">
+                <img class="clean img-responsive" src="<?php echo get_bloginfo('template_url') ?>/assets/img/comentariosm.jpg" style="padding: 0; margin: 0;" alt="La Iguana comentarios">
             </div>
             <div class="fb-comments" data-href="http://laiguana.tv<?php the_permalink() ?>"
                  data-width="100%" data-numposts="10" >
@@ -13,5 +14,3 @@
         </div>
     </div>
 </div>
-
-

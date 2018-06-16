3a2d927c37e2e01469d4aa5f5f5394d732ee4d72
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-06-16 10:59:22 -0400

    refactor home: banners and sections

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
-<?php 
-    get_header(); 
+<?php
+    get_header();
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
 ?>
@@ -7,7 +7,7 @@
 <?php get_template_part( "destacados"); ?>
 
 <div class="col-g">
-    <?php 
+    <?php
         $offset = 0;
         $amount = 1;
         $category = 'C1';
@@ -20,85 +20,101 @@
         include(locate_template('Nota.php'));
     ?>
     <div class="clearfix"></div>
-    <?php 
-        get_template_part( "HD-HM1");
-        get_template_part( "HD-HS2");
-
-        $amount = 4;        
-        $category = 'General';
-        include(locate_template('Nota.php'));
+    <?php
+        get_template_part( "banners/home/desktop/HD-N1");
+        get_template_part( "banners/home/desktop/HD-HS2");
     ?>
 
     <div class="clearfix"></div>
-    <?php 
-        get_template_part( "HD-HS3");
-    
-    ?>    
+    <?php
+        get_template_part( "banners/home/desktop/HD-HS3");
+    ?>
 </div>
 
 <div class="col-p">
-    <?php 
-        get_template_part( "HD-V1");
+    <?php
+        get_template_part( "banners/home/desktop/HD-V1");
         $imagesLoMasLeido = 0;
-        include(locate_template('lo_mas_leido.php'));               
-        get_template_part( "HD-C1"); 
-        get_template_part( "lo_mas_polemico"); 
+        include(locate_template('lo_mas_leido.php'));
     ?>
-    
 </div>
+
+
+<div class="col-g">
+    <?php
+        $amount = 4;
+        $category = 'General';
+        include(locate_template('Nota.php'));
+    ?>
+</div>
+
+
+<div class="col-p">
+    <?php
+        get_template_part( "banners/home/desktop/HD-C1");
+        get_template_part( "lo_mas_polemico");
+    ?>
+</div>
+
 
 <div class="clearfix"></div>
 
-<?php get_template_part( "HD-HL1"); ?>
+<?php get_template_part( "banners/home/desktop/HD-HL1"); ?>
 
 <div class="margin-top-10">
     <div class="col-g">
-        <?php 
-            get_template_part( "iguanazos");
-            $amount = 6;        
+        <?php get_template_part( "iguanazos"); ?>
+    </div>
+    <div class="col-p">
+        <?php get_template_part( "iguanazos_anteriores"); ?>
+    </div>
+    <div class="clearfix"></div>
+</div>
+
+<div class="margin-top-10">
+    <div class="col-g">
+        <?php
+            $amount = 6;
             $category = 'General';
             $offset = 4;
             include(locate_template('Nota.php'));
         ?>
-        
+
     </div>
     <div class="col-p">
-        <?php 
-            get_template_part( "iguanazos_anteriores");
-            get_template_part( "HD-C2");
-            get_template_part( "Cara_a_cara");             
-            get_template_part( "HD-V2");            
+        <?php
+            get_template_part( "banners/home/desktop/HD-C2");
+            get_template_part( "Cara_a_cara");
+            get_template_part( "banners/home/desktop/HD-V2");
         ?>
     </div>
-
     <div class="clearfix"></div>
-
-    <?php get_template_part( "HD-HL2"); ?>
+    <?php get_template_part( "banners/home/desktop/HD-HL2"); ?>
 </div>
 
 <?php get_template_part( "la-foto"); ?>
 
 <div class="margin-top-10">
     <div class="col-g">
-        <?php 
-            $amount = 6;        
+        <?php
+            $amount = 6;
             $category = 'General';
             $offset = 10;
             include(locate_template('Nota.php'));
         ?>
-        
+
     </div>
     <div class="col-p">
-        <?php 
-            get_template_part( "HD-C3");
+        <?php
+            get_template_part( "banners/home/desktop/HD-C3");
             get_template_part( "guerra_twitter");
-            get_template_part( "HD-V3");            
+            get_template_part( "banners/home/desktop/HD-V3");
         ?>
     </div>
 
     <div class="clearfix"></div>
 
-    <?php get_template_part( "HD-HL3"); ?>
+    <?php get_template_part( "banners/home/desktop/HD-HL3"); ?>
 </div>
 
 <div class="lomasjot el-video margin-top-10">
@@ -112,49 +128,49 @@
 
 <div class="clearfix"></div>
 
-<?php get_template_part( "HD-HB2"); ?>
+<?php get_template_part( "banners/home/desktop/HD-HB2"); ?>
 
 <div class="margin-top-10">
     <div class="col-g">
-        <?php 
-            $amount = 6;        
+        <?php
+            $amount = 6;
             $category = 'General';
             $offset = 16;
             include(locate_template('Nota.php'));?>
             <div class="clearfix"></div>
-        <?php get_template_part( "HD-HM4"); ?>
+        <?php get_template_part( "banners/home/desktop/HD-HM4"); ?>
     </div>
     <div class="col-p">
         <?php get_template_part( "lo_mas_reciente"); ?>
-        <?php get_template_part( "HD-V4"); ?>
+        <?php get_template_part( "banners/home/desktop/HD-V4"); ?>
     </div>
 </div>
 
 <div class="clearfix"></div>
 
-<?php 
-    get_template_part( "noticias_anteriores"); 
+<?php
+    get_template_part( "noticias_anteriores");
 ?>
 
 <div class="clearfix"></div>
 
 <?php
-    get_template_part( "HD-HL4"); 
-    get_template_part( "HM-C4");     
+    get_template_part( "banners/home/desktop/HD-HL4");
+    get_template_part( "banners/home/mobile/HM-C4");
 ?>
 
 <div class="trio" >
 <?php
-    get_template_part( "lo_mas_reciente_trio"); 
-    get_template_part( "lo_mas_polemico_trio"); 
-    get_template_part( "lo_mas_jot_trio"); 
+    get_template_part( "lo_mas_reciente_trio");
+    get_template_part( "lo_mas_polemico_trio");
+    get_template_part( "lo_mas_jot_trio");
 ?>
 </div>
 <div class="clearfix"></div>
 
 <?php
-    get_template_part( "HD-HL5"); 
-    get_template_part( "HM-HS7"); 
+    get_template_part( "banners/home/desktop/HD-HL5");
+    get_template_part( "banners/home/mobile/banners/home/mobile/HM-HS7");
 ?>
 
 <?php get_footer() ?>

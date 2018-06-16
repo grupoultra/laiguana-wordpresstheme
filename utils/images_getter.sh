23bb06dc4fb516f100232b11988326bc2ac2dd0c
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-06-15 23:39:58 -0400

    update the retrieving and renaming of images

--- utils/images_getter.sh
+++ utils/images_getter.sh
@@ -1,10 +1,10 @@
-mkdir ./wp-content/uploads/2017/08
-cd $_
+mkdir -p ./wp-content/uploads/2017/08
+cd ./wp-content/uploads/2017/08
 
-wget http://placehold.it/1070x441.png /
-cp 1070x441.png 1070x441-203x130.png
-cp 1070x441.png 1070x441-278x80.png
-cp 1070x441.png 1070x441-328x135.png
-cp 1070x441.png 1070x441-259x106.png
-cp 1070x441.png 1070x441-325x244.png
-cp 1070x441.png 1070x441-728x421.png
+wget http://placehold.it/1070x441.png
+wget http://placehold.it/203x130.png -O 1070x441-203x130.png
+wget http://placehold.it/278x80.png -O 1070x441-278x80.png
+wget http://placehold.it/328x135.png -O 1070x441-328x135.png
+wget http://placehold.it/259x106.png -O 1070x441-259x106.png
+wget http://placehold.it/325x244.png -O 1070x441-325x244.png
+wget http://placehold.it/728x421.png -O 1070x441-728x421.png

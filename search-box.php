ec938e2f14fe4b3d15e0e81f52caa15e05428a17
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-06-15 23:57:56 -0400

    add search-box

---
+++ search-box.php
@@ -0,0 +1,11 @@
+<div class="search-box">
+    <form action="http://www.google.co.ve" id="cse-search-box">
+        <div>
+            <input type="hidden" name="cx" value="partner-pub-8216476634474264:8992413038" />
+            <input type="hidden" name="ie" value="UTF-8" />
+            <input class="search-input" type="text" name="q" size="21"/>
+        </div>
+    </form>
+
+    <script type="text/javascript" src="http://www.google.co.ve/coop/cse/brand?form=cse-search-box&amp;lang=es"></script>
+</div>

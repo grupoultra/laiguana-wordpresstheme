3aef8244a66043cbea1652f1d9b45ce708311c38
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-06-15 23:57:46 -0400

    add social-nav

---
+++ social-nav.php
@@ -0,0 +1,50 @@
+
+<ul class="social-nav">
+  <li>
+    <a class="instagram" target="_blank" title="Instagram" href="http://instagram.com/laiguanatv">
+      <i class="fa fa-instagram"></i>
+      Instagram
+    </a>
+  </li>
+  <li>
+    <a class="youtube" target="_blank" title="Youtube" href="http://www.youtube.com/channel/UCCc2H9_eNnU7ucq2n7Sh3Rg">
+      <i class="fa fa-youtube-play"></i>
+      Youtube
+    </a>
+  </li>
+  <li>
+    <a class="facebook" target="_blank" title="Facebook" href="http://www.facebook.com/iguana.tv">
+      <i class="fa fa-facebook-square"></i>
+      Facebook
+    </a>
+  </li>
+  <li>
+    <a class="twitter" target="_blank" title="Twitter" href="http://www.twitter.com/la_iguanatv">
+      <i class="fa fa-twitter-square"></i>
+      Twitter
+    </a>
+  </li>
+  <li>
+    <a class="googleplus" target="_blank" title="Google Plus" href="https://plus.google.com/112720298192589737708">
+      <i class="fa fa-google-plus"></i>
+      Google Plus
+    </a>
+  </li>
+  <li>
+    <a class="pinterest" target="_blank" title="Pinterest" href="http://www.pinterest.com/laiguanatv">
+      <i class="fa fa-pinterest-p"></i>
+      Pinterest
+    </a>
+  </li>
+  <li>
+    <a class="rss" target="_blank" title="RSS" href="http://feeds.feedburner.com/laiguana-tv">
+      <i class="fa fa-rss"></i>
+      Feed
+    </a>
+  </li>
+
+
+
+
+
+</ul>

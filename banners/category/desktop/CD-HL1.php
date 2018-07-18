32078ac45d209e8c9312b7b5154c695bf739cc9b
Heiberth Pulgar <heiberthpulgar@gmail.com>
2018-07-18 17:46:06 -0400

    changing HB Banner category in category

--- banners/category/desktop/CD-HL1.php
+++
@@ -1,6 +0,0 @@
-<div id="HD-HL1" class="banner margin-top-10 banner-c banner970x90 hidden-mobile">
-    <script async type='text/javascript' src='//s.clickiocdn.com/t/common_258.js'></script>
-    <script class='__lxGc__' type='text/javascript'>
-        ((__lxGc__=window.__lxGc__||{'s':{},'b':0})['s']['_205553']=__lxGc__['s']['_205553']||{'b':{}})['b']['_609465']={'i':__lxGc__.b++};
-    </script>
-</div>

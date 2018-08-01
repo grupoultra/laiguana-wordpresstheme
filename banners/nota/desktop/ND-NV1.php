ef9e5c63526006c27b49c0659cb2f622dabf4772
Unknown <heiberthpulgar@gmail.com>
2018-08-01 18:12:23 -0400

    adding Embimedia ads to single

--- banners/nota/desktop/ND-NV1.php
+++ banners/nota/desktop/ND-NV1.php
@@ -1,3 +1,12 @@
 <div id="ND-NV1" class="banner margin-top-10 hidden-mobile">
-    <img src="<?php echo get_bloginfo('template_url') ?>/assets/img/banners/n300x600.gif" width="300px" height="600px" />
+        <div id="taboola-right-rail-thumbnails"></div>
+	<script type="text/javascript">
+	  window._taboola = window._taboola || [];
+	  _taboola.push({
+	    mode: 'thumbnails-rr',
+	    container: 'taboola-right-rail-thumbnails',
+	    placement: 'Right Rail Thumbnails',
+	    target_type: 'mix'
+	  });
+	</script>
 </div>

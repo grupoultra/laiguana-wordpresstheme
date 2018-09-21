dbaa50fdb45274995f18647c240287ec069347fc
Heiberth Pulgar <heiberthpulgar@gmail.com>
2018-09-21 08:53:04 -0400

    adding latinom ads

--- banners/home/desktop/HD-C1.php
+++ banners/home/desktop/HD-C1.php
@@ -1,6 +1,17 @@
 <div id="HD-C1" class="margin-top-10 banner banner300x250 hidden-mobile">
-    <script async type='text/javascript' src='//s.clickiocdn.com/t/common_258.js'></script>
-    <script class='__lxGc__' type='text/javascript'>
-    ((__lxGc__=window.__lxGc__||{'s':{},'b':0})['s']['_205553']=__lxGc__['s']['_205553']||{'b':{}})['b']['_614892']={'i':__lxGc__.b++};
-    </script>
+    <script type='text/javascript'><!--//<![CDATA[
+    var m3_u = (location.protocol=='https:'?'https://ads.latinongroup.com/delivery/ajs.php':'http://ads.latinongroup.com/delivery/ajs.php');
+    var m3_r = Math.floor(Math.random()*99999999999);
+    if (!document.MAX_used) document.MAX_used = ',';
+    document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
+    document.write ("?zoneid=2254");
+    document.write ('&amp;cb=' + m3_r);
+    if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
+    document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
+    document.write ("&amp;loc=" + escape(window.location));
+    if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
+    if (document.context) document.write ("&context=" + escape(document.context));
+    if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
+    document.write ("'><\/scr"+"ipt>");
+    //]]>--></script>
 </div>

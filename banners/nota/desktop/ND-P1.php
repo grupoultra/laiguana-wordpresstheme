b33c7a305be6f2d8a8cc1bce72df7acc998050d7
Heiberth Pulgar <heiberthpulgar@gmail.com>
2018-09-24 16:12:31 +0000

    fixing ND-P1

--- banners/nota/desktop/ND-P1.php
+++ banners/nota/desktop/ND-P1.php
@@ -1,3 +1,4 @@
 <div id="ND-P1">
     <script>(function(){var i='cucR_4s1gvK2_VYGVIE12ct7XnLnE0PT8uzmujZ4DZh9hlw6PlZV';document.write('<div id="'+i+'"></div>');(playerPro=window.playerPro||[]).push(i);})();</script>
 </div>
+
